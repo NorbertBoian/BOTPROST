6be76a7751cbf834bf7e6fb933bb38568c3e6d4e
Andrei Stoica <a.stoica@rosoftlab.net>
2020-12-02 21:14:10 +0200

    musica domle

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { PREFIX, TOKEN } = require("./config.json");
 const Discord = require("discord.js");
-
+const ytdl = require("ytdl-core");
 const client = new Discord.Client();
 client.login(TOKEN);
 
-client.on("message", (message) => {
+client.on("message", async (message) => {
   if (message.author.bot || !message.content.startsWith(PREFIX)) return; //verificam sa nu fie bot si sa fie prefixul
 
   const args = message.content.slice(PREFIX.length).split(" "); // despartim dupa spatiu
@@ -12,5 +12,19 @@ client.on("message", (message) => {
   switch (args[0].toLowerCase()) {
     case "ping":
       message.channel.send("pong!").catch(console.error);
+      break;
+    case "join":
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+
+        connection.play(
+          ytdl("https://www.youtube.com/watch?v=dQw4w9WgXcQ", {
+            filter: "audioonly",
+          })
+        );
+      } else {
+        message.reply("You need to join a voice channel first!");
+      }
+      break;
   }
 });
