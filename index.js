7b61a558080bcdd74d824034cf6d9ab4ba4b62b1
Andrei Stoica <a.stoica@rosoftlab.net>
2020-12-02 20:51:34 +0200

    stergem ce o facut norbi in typescript

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const {PREFIX, TOKEN} = require('./config.json');
-const Discord = require('discord.js');
+const { PREFIX, TOKEN } = require("./config.json");
+const Discord = require("discord.js");
 
 const client = new Discord.Client();
 client.login(TOKEN);
 
-client.on('message', message =>
-{
-    if(message.author.bot || !message.content.startsWith(PREFIX)) return; //verificam sa nu fie bot si sa fie prefixul
+client.on("message", (message) => {
+  if (message.author.bot || !message.content.startsWith(PREFIX)) return; //verificam sa nu fie bot si sa fie prefixul
 
-    const args = message.content.slice(PREFIX.length).split(' ');// despartim dupa spatiu
+  const args = message.content.slice(PREFIX.length).split(" "); // despartim dupa spatiu
 
-    switch (args[0].toLowerCase()){
-        case 'ping':
-            message.channel.send('pong!').catch(console.error);
-    }
-})
+  switch (args[0].toLowerCase()) {
+    case "ping":
+      message.channel.send("pong!").catch(console.error);
+  }
+});
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-require = require("esm")(module);
-module.exports = require("./bot.ts");
