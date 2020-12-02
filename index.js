0fd9582c9e3dc8fccdc37b3b83c21e6c336560d7
Andrei Stoica <a.stoica@rosoftlab.net>
2020-12-02 20:20:29 +0200

    add comments

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ client.on('message', message =>
 {
     if(message.author.bot || !message.content.startsWith(PREFIX)) return; //verificam sa nu fie bot si sa fie prefixul
 
-    const args = message.content.slice(PREFIX.length).split(' ');
+    const args = message.content.slice(PREFIX.length).split(' ');// despartim dupa spatiu
 
-    console.log(args[0]);
     switch (args[0].toLowerCase()){
         case 'ping':
             message.channel.send('pong!').catch(console.error);
