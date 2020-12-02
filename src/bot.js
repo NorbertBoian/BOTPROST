e5a193a32a5f3c5ae9be156e4c5f4c1f3e555e8e
Bojan <norbertboian@gmail.com>
2020-12-02 22:32:54 +0200

    imirupeureachea

--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,24 @@
 import dotenv from "dotenv";
 import Discord from "discord.js";
-import ytdl from "ytdl-core";
 const client = new Discord.Client();
+import ytdl from "ytdl-core";
+import ytpl from "ytpl";
 dotenv.config();
-client.login(procces.env.TOKEN);
+client.login(process.env.TOKEN);
+var py;
+ytpl("PL36384B2DAC7D315B")
+  .then((playlist) => {
+    py = playlist.items;
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 client.on("message", async (message) => {
-  if (message.author.bot || !message.content.startsWith(procces.env.PREFIX))
-    return; //verificam sa nu fie bot si sa fie prefixul
+  if (message.author.bot || !message.content.startsWith(process.env.PREFIX))
+    return; //verificam sa nu fie bot si sa fie process.env.PREFIXul
 
-  const args = message.content.slice(procces.env.PREFIX.length).split(" "); // despartim dupa spatiu
+  const args = message.content.slice(process.env.PREFIX.length).split(" "); // despartim dupa spatiu
 
   switch (args[0].toLowerCase()) {
     case "ping":
@@ -17,12 +26,21 @@ client.on("message", async (message) => {
       break;
     case "join":
       if (message.member.voice.channel) {
+        let index = 0;
         const connection = await message.member.voice.channel.join();
         const dispatcher = connection.play(
-          ytdl("https://www.youtube.com/watch?v=H9154xIoYTA", {
+          ytdl(py[index].url, {
             filter: "audioonly",
           })
         );
+        dispatcher.on("finish", () => {
+          index++;
+          const dispatcher = connection.play(
+            ytdl(py[index].url, {
+              filter: "audioonly",
+            })
+          );
+        });
       } else {
         message.reply("You need to join a voice channel first!");
       }
