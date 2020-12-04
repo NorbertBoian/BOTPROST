29f2fe06366d380941fd9f7ecdab1e541dee357f
Andrei Stoica <a.stoica@rosoftlab.net>
2020-12-04 19:14:04 +0200

    ceva logica de quiz

--- src/bot.js
+++ src/bot.js
@@ -3,93 +3,104 @@ import Discord from "discord.js";
 const client = new Discord.Client();
 import ytdl from "ytdl-core";
 import ytpl from "ytpl";
+import stringSimilarity from "string-similarity";
 dotenv.config();
 console.log("Running");
-
-// var py;
-// ytpl("PL36384B2DAC7D315B")
-//   .then((playlist) => {
-//     py = playlist.items;
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+let isQuiz = false;
+let players;
+let nowPlaying;
 
 client.on("message", async (message) => {
   try {
-    if (message.author.bot || !message.content.startsWith(process.env.PREFIX))
-      return; //verificam sa nu fie bot si sa fie prefix
-
-    const args = message.content.slice(process.env.PREFIX.length).split(" "); // despartim dupa spatiu
-    const memberVoiceChannel = message.member.voice.channel;
-    const memberTextChannel = message.channel;
-    switch (args[0].toLowerCase()) {
-      case "ping":
-        memberTextChannel.send("pong!");
-        break;
-      case "join":
-        memberVoiceChannel.join();
-        break;
-      case "leave":
-        memberVoiceChannel.leave();
-        break;
-      case "play":
-        // const connection = await memberVoiceChannel.join();
-        // const url = args[1];
-        // if (url.includes("list")) {
-        //   const response = await ytpl(url);
-        //   const items = response.items;
-        //   const urls = items.map((item) => item.url);
-        //   const finish = () => {
-        //     urls.shift();
-        //     play();
-        //   };
-        //   const play = () => {
-        //     console.log(urls[0]);
-        //     const dispatcher = connection.play(
-        //       ytdl(urls[0], {
-        //         filter: "audioonly",
-        //       })
-        //     );
-        //     dispatcher.on("finish", finish);
-        //   };
-        //   play();
-        // } else {
-        //   connection.play(
-        //     ytdl(url, {
-        //       filter: "audioonly",
-        //     })
-        //   );
-        // }
-        break;
-      case "quiz":
-        const connection = await memberVoiceChannel.join();
-        const url = args[1];
-        if (url.includes("list")) {
-          const response = await ytpl(url);
-          const items = response.items;
-          const urls = items.map((item) => item.url);
-          let index;
-          const finish = () => {
-            urls.splice(index, 1);
+    if (
+      message.author.bot ||
+      !message.content.startsWith(process.env.PREFIX) ||
+      isQuiz
+    )
+      return; //verificam sa nu fie bot sau sa fie prefix
+    if (isQuiz && !message.content.startsWith(process.env.PREFIX)) {
+      const string = message.content.toLowerCase();
+      return;
+    }
+    if (message.content.startsWith(process.env.PREFIX)) {
+      console.log("aici");
+      const args = message.content.slice(process.env.PREFIX.length).split(" "); // despartim dupa spatiu
+      const memberVoiceChannel = message.member.voice.channel;
+      const memberTextChannel = message.channel;
+      switch (args[0].toLowerCase()) {
+        case "ping":
+          memberTextChannel.send("pong!");
+          break;
+        case "join":
+          memberVoiceChannel.join();
+          break;
+        case "leave":
+          memberVoiceChannel.leave();
+          break;
+        case "play":
+          // const connection = await memberVoiceChannel.join();
+          // const url = args[1];
+          // if (url.includes("list")) {
+          //   const response = await ytpl(url);
+          //   const items = response.items;
+          //   const urls = items.map((item) => item.url);
+          //   const finish = () => {
+          //     urls.shift();
+          //     play();
+          //   };
+          //   const play = () => {
+          //     console.log(urls[0]);
+          //     const dispatcher = connection.play(
+          //       ytdl(urls[0], {
+          //         filter: "audioonly",
+          //       })
+          //     );
+          //     dispatcher.on("finish", finish);
+          //   };
+          //   play();
+          // } else {
+          //   connection.play(
+          //     ytdl(url, {
+          //       filter: "audioonly",
+          //     })
+          //   );
+          // }
+          break;
+        case "quiz":
+          isQuiz = true;
+          players = memberVoiceChannel.members
+            .map((guild) => guild.user)
+            .filter((user) => !user.bot);
+          const connection = await memberVoiceChannel.join();
+          const url = args[1];
+          if (url.includes("list")) {
+            const response = await ytpl(url);
+            const items = response.items;
+            let index;
+            const finish = () => {
+              items.splice(index, 1);
+              play();
+            };
+            const play = () => {
+              index = Math.floor(Math.random() * items.length);
+              nowPlaying = items[index];
+              //console.log(urls[index]);
+              const dispatcher = connection.play(
+                ytdl(items[index].url, {
+                  filter: "audioonly",
+                })
+              );
+              dispatcher.on("finish", finish);
+            };
             play();
-          };
-          const play = () => {
-            index = Math.floor(Math.random() * urls.length);
-            console.log(index + " din " + urls.length);
-            //console.log(urls[index]);
-            const dispatcher = connection.play(
-              ytdl(urls[index], {
-                filter: "audioonly",
-              })
-            );
-            dispatcher.on("finish", finish);
-          };
-          play();
-        } else {
-          memberTextChannel.send("trebe playlist bo$$");
-        }
-        break;
+          } else {
+            memberTextChannel.send("trebe playlist bo$$");
+          }
+          break;
+        default:
+          memberTextChannel.send("nu stiu deastea bo$$");
+          break;
+      }
     }
   } catch (error) {
     console.error(error);
